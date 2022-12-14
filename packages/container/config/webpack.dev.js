f5b144bf4f87513777efcf89e46fb6b6336184b7
Aniket Kumar <aniketkumar049@gmail.com>
2022-12-14 09:01:48 +0530

    Fixing history fallback

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -9,8 +9,9 @@ const devConfig = {
   devServer: {
     port: 8080,
     historyApiFallback: {
-      index: "index.html",
+      index: "/index.html",
     },
+    // or historyApiFallback: true,
   },
   plugins: [
     new ModuleFederationPlugin({
