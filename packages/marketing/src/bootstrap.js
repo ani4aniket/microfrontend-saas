091cb14e16c00fa5f69c25b20676bfe9a0dfd739
Aniket Kumar <aniketkumar049@gmail.com>
2022-12-16 09:56:45 +0530

    Implement browser history and memory history

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathName) {
+        history.push(nextPathName);
+      }
+    },
+  };
 };
 
 // If we are in development and in isolation,
@@ -12,7 +28,7 @@ const mount = (el) => {
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
